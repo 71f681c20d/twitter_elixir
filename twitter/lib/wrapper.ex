c373d5125118046c5a2578f119eda425e30c83d0
alexwb1 <abarnard1@ufl.edu>
2019-11-18 16:53:59 -0500

    fixed mnesia code

--- twitter/lib/wrapper.ex
+++ twitter/lib/wrapper.ex
@@ -5,15 +5,9 @@ defmodule Wrapper do # wraps a centralized ETS instance
     :mnesia.create_schema([node()])
     :mnesia.start
     # A tweet object is like this: tweet = {user1, "mytweet #hello @user2", {#hello}, {@user2}}
-    {_, :ok} = :mnesia.create_table(hashtags, [ # [#i_luv_twitter, {tweet object 1, tweet object 2, ...}]
-                  attributes: [:hashtag, :tweets]
-                  type: :set])
-    {_, :ok} = :mnesia.create_table(mentions, [
-                  attributes: [:mention, :tweets]
-                  type: :set])
-    {_, :ok} = :mnesia.create_table(social_graph, [
-                  attributes: [:id, :followed_by]
-                  type: :set])
+    {_, :ok} = :mnesia.create_table(:hashtags, [  attributes: [:hashtag, :tweets], type: :set]) # [#i_luv_twitter, {tweet object 1, tweet object 2, ...}]
+    {_, :ok} = :mnesia.create_table(:mentions, [ attributes: [:mention, :tweets], type: :set])
+    {_, :ok} = :mnesia.create_table(:social_graph, [ attributes: [:id, :followed_by], type: :set])
 
     # :ets.new(:wrapper, [
     #   :set,
@@ -27,6 +21,7 @@ defmodule Wrapper do # wraps a centralized ETS instance
   end
 
   def start_link(arg) do
+    # TODO: initialize schema for DBMS
     GenServer.start_link(__MODULE__, arg, name: __MODULE__)
   end
 
@@ -35,25 +30,25 @@ defmodule Wrapper do # wraps a centralized ETS instance
   def get_social_graph(key), do: get(key, :social_graph)
 
   def get(key, table) do
-    from_pid = elem(Map.fetch(from, :pid), 1)
-    GenServer.cast(from_pid, {:get, key, table})
+    # from_pid = elem(Map.fetch(from, :pid), 1)
+    GenServer.cast(self, {:get, key, table})
   end
 
   def handle_cast({:get, key, table}, state) do
     :mnesia.read({table, key})
   end
 
-  def put_hashtags(key, value), do: get(key, value, :hashtags)
-  def put_mentions(key, value), do: get(key, value, :mentions)
-  def put_social_graph(key, value), do: get(key, value, :social_graph)
+  def put_hashtags(key, value), do: put(key, value, :hashtags)
+  def put_mentions(key, value), do: put(key, value, :mentions)
+  def put_social_graph(key, value), do: put(key, value, :social_graph)
 
   def put(key, value, table) do
-    from_pid = elem(Map.fetch(from, :pid), 1)
-    GenServer.call(from_pid, {:put, key, value, table})
+    # from_pid = elem(Map.fetch(from, :pid), 1)
+    GenServer.call(self, {:put, key, value, table})
   end
 
   def handle_call({:put, key, value, table}, state) do
     :mnesia.write({table, key, value})
-    -> :ok
+    :ok
   end
 end
