c77cd879a7c11b483419eed388a850d861916076
alexwb1 <abarnard1@ufl.edu>
2019-11-24 23:06:28 -0500

    pushing changes

--- twitter/test/twitter_test.exs
+++ twitter/test/twitter_test.exs
@@ -15,14 +15,23 @@ defmodule TwitterTest do
       Mention
     Deliver twwets live if the user is connected
   """
-
+  # setup_all do
+  #   for i <- [1..10], do: Engine.join_twitter("myusername"+Integer.to_string(i))
+  #   for i <- [1..10] do # everyone follows 3 random ppl
+  #     Engine.follow_user("myusername"+Integer.to_string(i), :rand.uniform(10))
+  #     Engine.follow_user("myusername"+Integer.to_string(i), :rand.uniform(10))
+  #     Engine.follow_user("myusername"+Integer.to_string(i), :rand.uniform(10))
+  #   end
+  #   for i <- [1..9], do: Engine.receive_tweet("mentioning @username"+Integer.to_string(:rand.uniform(10))+"and twweting about #hashtag"+Integer.to_string(:rand.uniform(10)))
+  #   {:ok}
+  # end
 
   test "register new account" do
-    assert {_reply, :ok, _state} = Engine.join_twitter("myusername1")
+    assert {_reply, :ok, _state} = Engine.join_twitter("myusername")
   end
 
   test "Delete account" do
-    assert {_reply, :ok, _state} = Engine.join_twitter("myusername1")
+    assert {_reply, :ok, _state} = Engine.join_twitter("myusername")
   end
 
   test "Send a tweet" do
@@ -35,7 +44,8 @@ defmodule TwitterTest do
   end
 
   test "Re-tweet" do
-    assert {_reply, :ok, _state} = Engine.retweet(uid, tweet_id) # TODO fix this
+    assert false
+    # assert {_reply, :ok, _state} = Engine.retweet(uid, tweet_id) # TODO fix this
   end
 
   test "query: Subscribed-to" do
